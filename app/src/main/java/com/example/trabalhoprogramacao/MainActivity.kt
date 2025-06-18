184272ca0c4fd1ed671f22b4f2605b1e198b88b4
Rafadex42 <rochamiguelbruno@gmail.com>
2025-06-18 18:53:00 +0100

    Atualizacao/Modificacao Inicial

--- app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
+++ app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
@@ -1,47 +1,20 @@
 package com.example.trabalhoprogramacao
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.trabalhoprogramacao.ui.theme.TrabalhoProgramacaoTheme
+import androidx.compose.runtime.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TrabalhoProgramacaoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                        asdasd
-                }
-            }
+            TemperatureConverter()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TemperatureConverter() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabalhoProgramacaoTheme {
-        Greeting("Android")
-    }
 }
