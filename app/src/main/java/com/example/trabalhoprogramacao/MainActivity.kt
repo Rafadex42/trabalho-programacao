a3ebbf9a7519b6aa1163912f037f50d37c6fb53f
Rafadex42 <rochamiguelbruno@gmail.com>
2025-06-18 21:24:20 +0100

    Insercao de input para temperatura

--- app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
+++ app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
@@ -3,7 +3,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TemperatureConverter() {
+    var inputText by remember { mutableStateOf("") }
 
-
+    MaterialTheme {
+        Surface( modifier = Modifier.fillMaxSize() ) {
+            Column() {
+                TextField(
+                    value = inputText,
+                    onValueChange = { inputText = it },
+                    label = { Text("Coloque a temperatura") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
 }
