7b921f34cd3e3fcb0770e8ad585b651a9b0bcdc6
Rafadex42 <rochamiguelbruno@gmail.com>
2025-06-23 14:55:47 +0100

    Organização de todo a coluna com espacamentos entre botoes etc (o codigo é o mesmo do ultimo commit mas enviei uma descricao repetida sem querer)

--- app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
+++ app/src/main/java/com/example/trabalhoprogramacao/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
